9e97611144f1281bcaf7fdb60501d8bd8f5d5b7d
Vagus-art <agustin.favo@gmail.com>
2020-09-23 14:11:48 -0300

    mejorada compatibilidad con js

--- public/js/setForm.js
+++ public/js/setForm.js
@@ -1,11 +1,10 @@
-let updateNoteFormData = {
+var updateNoteFormData = {
     title: "",
     content: "",
     id: 0,
 };
-
-const modalFormTitleInput = document.getElementById("modalFormTitleInput");
-const modalFormContentInput = document.getElementById("modalFormContentInput");
+var modalFormTitleInput = document.getElementById("modalFormTitleInput");
+var modalFormContentInput = document.getElementById("modalFormContentInput");
 
 function setForm(note) {
     updateNoteFormData = {
@@ -34,6 +33,10 @@ function saveNote() {
         url: "/",
         data: updateNoteFormData,
     })
-        .then(() => window.location.reload())
-        .catch((err) => console.log(err));
+        .then(function () {
+            return window.location.reload();
+        })
+        ["catch"](function (err) {
+            return console.log(err);
+        });
 }
